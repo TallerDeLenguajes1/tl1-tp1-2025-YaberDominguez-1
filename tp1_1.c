aaac192c414084fc3bb6c99b792bedcf09a1eb08
Yaber Dominguez <yaberdamianmariadominguezbaez@gmail.com>
2026-04-13 17:50:26 -0300

    PUNTO 4e terminado

--- tp1_1.c
+++ tp1_1.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-void main()
+int main()
 {
     printf("\n Hola Mundo");
-    
+    int a = 1056;
+    int *p = &a;
+    printf("\nContenido de p: %d", *p);
+    printf("\nDireccion que apunta p: %p", p);
+    printf("\nDireccion de memoria de a: %p", &a);
+    printf("\nDireccion de memoria de p: %p", &p);
+    printf("\nTamanio de memoria de a: %d",sizeof(a));
+
+    return 0;
 }
